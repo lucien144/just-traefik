dfbde9eaf36238ecf1317d14ddfb8f8c8ec5600b
lucien144 <honza@lucien144.net>
2023-09-29 19:41:23 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+docker network create web
+touch acme.json
+chmod 600 acme.json
+
+# Stop all containers
+docker stop traefik
+
+# Remove all containers
+docker rm traefik
+
+docker run -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $PWD/traefik.toml:/traefik.toml \
+  -v $PWD/traefik_dynamic.toml:/traefik_dynamic.toml \
+  -v $PWD/acme.json:/acme.json \
+  -v $PWD/ssl:/ssl \
+  -p 80:80 \
+  -p 443:443 \
+  --restart=always \
+  --network web \
+  --name traefik \
+  traefik:v2.10
